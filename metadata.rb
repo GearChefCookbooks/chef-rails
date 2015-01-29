3a16c215c930aacd69a87f442262d0eae6ab31ab
Gary Leong <gwleong@gmail.com>
2015-01-29 22:54:39 +0000

    updated the version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'austen.dev@gmail.com'
 license          'MIT License'
 description      'example cookbook used to provision example docker containers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
