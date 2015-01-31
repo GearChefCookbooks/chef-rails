cbceb5b2cc51f210fe1aa0c8481798dfad17afcc
Gary Leong <gwleong@gmail.com>
2015-01-31 08:33:25 +0000

    some metadata changes

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,4 @@ description      'example cookbook used to provision example docker containers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.3'
 
-
-
 depends "docker"
